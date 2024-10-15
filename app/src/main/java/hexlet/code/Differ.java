c82087798277912a591186c2dc9c42855b93b2ac
Mikhail Dedov <mdedov@gmail.com>
2024-10-15 08:45:55 +0400

    fix checkstyle

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,9 +1,5 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
