fc4f607a5fdbd15bf43a0d9486f36f6e614bd1c3
Mikhail Dedov <mdedov@gmail.com>
2024-12-02 18:36:20 +0400

    fix DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -26,18 +26,29 @@ public class DifferTest {
     }
 
     @Test
-    void testGenerate() throws Exception {
-        var filepath1 = getFixturePath("filepath1.json").toString();
-        var filepath2 = getFixturePath("filepath2.json").toString();
+    void testGeneratePlainFromJson() throws Exception {
+        var filepath1 = getFixturePath("filenested1.json").toString();
+        var filepath2 = getFixturePath("filenested2.json").toString();
 
-        String actual = Differ.generate(filepath1, filepath2, "stylish");
-        String expected = Files.readString(getFixturePath("resultstylish.txt"));
+        String actual = Differ.generate(filepath1, filepath2, "plain");
+        String expected = readFixture("resultplain.txt");
 
         assertEquals(expected, actual);
     }
 
     @Test
-    void testGenerateNested() throws Exception {
+    void testGeneratePlainFromYml() throws Exception {
+        var filepath1 = getFixturePath("filenested1.yml").toString();
+        var filepath2 = getFixturePath("filenested2.yml").toString();
+
+        String actual = Differ.generate(filepath1, filepath2, "plain");
+        String expected = readFixture("resultplain.txt");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGenerateStylishFromJson() throws Exception {
         var filepath1 = getFixturePath("filenested1.json").toString();
         var filepath2 = getFixturePath("filenested2.json").toString();
 
@@ -48,7 +59,18 @@ public class DifferTest {
     }
 
     @Test
-    void testGenerateJsonFormat() throws Exception {
+    void testGenerateStylishFromYml() throws Exception {
+        var filepath1 = getFixturePath("filenested1.yml").toString();
+        var filepath2 = getFixturePath("filenested2.yml").toString();
+
+        String actual = Differ.generate(filepath1, filepath2, "stylish");
+        String expected = readFixture("resultnestedstylish.txt");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGenerateJsonFromJson() throws Exception {
         var file1Path = getFixturePath("filenested1.json").toString();
         var file2Path = getFixturePath("filenested2.json").toString();
 
@@ -76,9 +98,48 @@ public class DifferTest {
     }
 
     @Test
-    void testGenerateDefault() throws Exception {
+    void testGenerateJsonFromYml() throws Exception {
+        var filePath1 = getFixturePath("filenested1.yml").toString();
+        var filePath2 = getFixturePath("filenested2.yml").toString();
+
+        String actualJsonOutputStr = Differ.generate(filePath1, filePath2, "json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Map<String, Object>> actualJsonOutput = objectMapper.readValue(
+                actualJsonOutputStr, new TypeReference<List<Map<String, Object>>>() { }
+        );
+
+        String expectedJsonOutputStr = readFixture("resultjson.json");
+
+        List<Map<String, Object>> expectedJsonOutput = objectMapper.readValue(
+                expectedJsonOutputStr, new TypeReference<List<Map<String, Object>>>() { }
+        );
+
+        List<Map<String, Object>> sortedExpected = expectedJsonOutput.stream()
+                .sorted(Comparator.comparing(map -> map.get("key").toString()))
+                .collect(Collectors.toList());
+        List<Map<String, Object>> sortedActual = actualJsonOutput.stream()
+                .sorted(Comparator.comparing(map -> map.get("key").toString()))
+                .collect(Collectors.toList());
+
+        assertEquals(sortedExpected, sortedActual);
+    }
+
+    @Test
+    void testGenerateDefaultFromJson() throws Exception {
         var filepath1 = getFixturePath("filepath1.json").toString();
         var filepath2 = getFixturePath("filepath2.json").toString();
+
+        String actual = Differ.generate(filepath1, filepath2);
+        String expected = Files.readString(getFixturePath("resultstylish.txt")).trim();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGenerateDefaultFromYml() throws Exception {
+        var filepath1 = getFixturePath("filepath1.yml").toString();
+        var filepath2 = getFixturePath("filepath2.yml").toString();
 
         String actual = Differ.generate(filepath1, filepath2);
         String expected = Files.readString(getFixturePath("resultstylish.txt")).trim();
