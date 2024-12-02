f1a570ac31fc32f805c316104601216d6107e4f4
Mikhail Dedov <mdedov@gmail.com>
2024-12-02 15:20:25 +0400

    fix JsonFormatter

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -1,7 +1,6 @@
 package hexlet.code.formatters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import hexlet.code.DiffEntry;
 import hexlet.code.FormatterSelection;
 
@@ -11,16 +10,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class JsonFormatter implements FormatterSelection {
-    private final ObjectMapper objectMapper = new ObjectMapper()
-            .enable(SerializationFeature.INDENT_OUTPUT) // Включаем pretty-print
-            .configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true); // Сортировка полей по ключам
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public final String format(List<DiffEntry> diff) throws Exception {
         List<Map<String, Object>> formattedDiff = diff.stream()
-                .sorted((entry1, entry2) -> entry1.getKey().compareTo(entry2.getKey())) // Сортировка по ключу
+                .sorted((entry1, entry2) -> entry1.getKey().compareTo(entry2.getKey()))
                 .map(entry -> {
-                    // Создаем Map через HashMap для поддержки null значений
                     Map<String, Object> map = new HashMap<>();
                     map.put("key", entry.getKey());
                     map.put("type", entry.getType().toString().toLowerCase());
